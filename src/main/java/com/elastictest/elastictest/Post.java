e557abf364953362478349ff45c56c9460d57bc0
LEEForgiveness <121328409+LEEForgiveness@users.noreply.github.com>
2024-06-19 17:47:16 +0900

    mu

--- src/main/java/com/elastictest/elastictest/Post.java
+++ src/main/java/com/elastictest/elastictest/Post.java
@@ -31,4 +31,6 @@ public class Post {
 
 	private LocalDateTime updatedAt;
 
+	private List<String> keywords;
+
 }
--- src/main/java/com/elastictest/elastictest/PostDocument.java
+++ src/main/java/com/elastictest/elastictest/PostDocument.java
@@ -2,6 +2,7 @@ package com.elastictest.elastictest;
 
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,7 +12,9 @@ import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.InnerField;
 import org.springframework.data.elasticsearch.annotations.Mapping;
+import org.springframework.data.elasticsearch.annotations.MultiField;
 import org.springframework.data.elasticsearch.annotations.Setting;
 
 @Getter
@@ -20,15 +23,22 @@ import org.springframework.data.elasticsearch.annotations.Setting;
 @Builder
 @Document(indexName = "auctionpost")
 @TypeAlias("auctionpost")
-//@Setting(settingPath = "elasticsearch/settings/settings.json")
-//@Mapping(mappingPath = "elasticsearch/settings/mappings.json")
+@Setting(settingPath = "elasticsearch/settings/settings.json")
 public class PostDocument {
 
 	@Id
 	@Field(type = FieldType.Long)
 	private Long id;
 
-	@Field(type = FieldType.Text, analyzer = "nori")
+//	@MultiField(
+//		mainField = @Field(type = FieldType.Text, copyTo = "keywords"),
+//		otherFields = {
+//			@InnerField(suffix = "nori", type = FieldType.Text, analyzer = "my_nori_analyzer", searchAnalyzer = "my_nori_analyzer"),
+//			@InnerField(suffix = "ngram", type = FieldType.Text, analyzer = "my_ngram_analyzer", searchAnalyzer = "my_ngram_analyzer")
+//		}
+//	)
+	@Field(type = FieldType.Text, analyzer = "my_nori_analyzer", searchAnalyzer = "my_nori_analyzer")
+//	@Field(type = FieldType.Text, analyzer = "my_ngram_analyzer", searchAnalyzer = "my_ngram_analyzer")
 	private String title;
 
 	@Field(type = FieldType.Text, analyzer = "nori")
@@ -39,9 +49,6 @@ public class PostDocument {
 
 	@Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
 	private LocalDateTime updatedAt;
-
-	@Field(type = FieldType.Text, analyzer = "nori")
-	private String keywords;
 
 	public static PostDocument from(Post post) {
 		return PostDocument.builder()
